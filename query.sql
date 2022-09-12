df94ace7520ea288b72989a3542f7285366b067e
Janitor <data_janitor@none.local>
2022-09-12 21:36:44 +0800

    Bugfix: add proper simp to trad conversion
    Also filter out honorary Hong Kong people (usually a honorary degree or membership in a HK institution eg. university)

--- query.sql
+++ query.sql
@@ -2,5 +2,5 @@ SELECT page_title from page where page_id IN
     (SELECT c1.cl_from
         from categorylinks as c1
         join categorylinks as c2 on c1.cl_from = c2.cl_from
-        WHERE c1.cl_to = 'YYYY年出生' AND c2.cl_to LIKE '%香港%'
+        WHERE c1.cl_to = 'YYYY年出生' AND c2.cl_to LIKE '%香港%' AND (NOT c2.cl_to LIKE '%香港%榮譽%')
     );
