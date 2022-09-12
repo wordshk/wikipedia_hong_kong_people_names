5beb68b16f1881adc18a7484a7f1aa4a8009f5b6
Janitor <data_janitor@none.local>
2022-09-13 00:15:03 +0800

    Minor fixes
    1. Exclude more people with honorary links to Hong Kong
    2. Fix 涂 (originally was translated to 塗 during the simp->trad conversion

--- query.sql
+++ query.sql
@@ -2,5 +2,5 @@ SELECT page_title from page where page_id IN
     (SELECT c1.cl_from
         from categorylinks as c1
         join categorylinks as c2 on c1.cl_from = c2.cl_from
-        WHERE c1.cl_to = 'YYYY年出生' AND c2.cl_to LIKE '%香港%' AND (c2.cl_to = '獲頒授香港榮譽勳章者' OR (NOT c2.cl_to LIKE '%香港%榮譽%'))
+        WHERE c1.cl_to = 'YYYY年出生' AND c2.cl_to LIKE '%香港%' AND (c2.cl_to = '獲頒授香港榮譽勳章者' OR (NOT c2.cl_to LIKE '%香港%名譽%' AND NOT c2.cl_to LIKE '%香港%榮譽%'))
     );
