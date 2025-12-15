b9818ad2ddf2a1da92221e633f9dc45fdd2b1df0
Janitor <data_janitor@none.local>
2025-12-15 16:29:51 +0800

    Update the script due to schema changes
    According to https://www.mediawiki.org/wiki/Manual:Categorylinks_table , the cl_to field was changed to cl_target_id, so we need to join the linktarget table as well.

--- query.sql
+++ query.sql
@@ -1,6 +1,11 @@
 SELECT page_title from page where page_id IN
     (SELECT c1.cl_from
         from categorylinks as c1
+        join linktarget as l1 on c1.cl_target_id = l1.lt_id
+
         join categorylinks as c2 on c1.cl_from = c2.cl_from
-        WHERE c1.cl_to = 'YYYY年出生' AND c2.cl_to LIKE '%香港%' AND (c2.cl_to = '獲頒授香港榮譽勳章者' OR (NOT c2.cl_to LIKE '%香港%名譽%' AND NOT c2.cl_to LIKE '%香港%榮譽%'))
+        join linktarget as l2 on c2.cl_target_id = l2.lt_id
+
+
+        WHERE l1.lt_title = 'YYYY年出生' AND l2.lt_title LIKE '%香港%' AND (l2.lt_title = '獲頒授香港榮譽勳章者' OR (NOT l2.lt_title LIKE '%香港%名譽%' AND NOT l2.lt_title LIKE '%香港%榮譽%'))
     );
